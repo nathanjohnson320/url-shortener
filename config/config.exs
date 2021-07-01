f7dc94dfbd34eb671e8da80c9222c94d035bfa22
Nathan Johnson <nathanjohnson320@gmail.com>
2021-07-01 15:35:48 -0400

    import Config not Mix.Config

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,11 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# and its dependencies with the aid of the Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
 
 # General application configuration
-import Mix.Config
+import Config
 
 config :url_shortener,
   ecto_repos: [UrlShortener.Repo]
